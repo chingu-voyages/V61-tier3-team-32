ee1191fe518e35ea91391c6985b222ea796b494f
DavidBugger <bwehfukdavid@gmail.com>
2026-06-26 20:30:15 +0100

    Allow Vercel frontend origin in CORS

--- server/src/index.js
+++ server/src/index.js
@@ -10,6 +10,9 @@ const app = express();
 
 const allowedOrigins = [
   process.env.CLIENT_URL,
+  process.env.FRONTEND_URL,
+  "https://foodrescue-deploy-client.vercel.app",
+  "https://foodrescue-deploy-server.vercel.app",
   "http://localhost:5173",
   "http://127.0.0.1:5173",
   "http://localhost:3000",
